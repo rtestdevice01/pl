3cb812590261a951e78fb3561b84e58ba2caec20
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-09-10 10:50:41 -0400

    Show contents of Info.plist

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,3 +5,8 @@ then
     plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/reactnativepipeline55/Info.plist
 fi
 plutil -replace CFBundleIdentifier -string "io.rangle.reactnativepipeline" $APPCENTER_SOURCE_DIRECTORY/reactnativepipeline55/Info.plist
+
+echo "POTATO 1"
+cat ios/reactnativepipeline55/Info.plist
+
+false
