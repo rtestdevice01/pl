7a40a8004e4cdd234c1ed2cf2acafd7c674a7305
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-09-10 10:20:56 -0400

    Added pre-build step

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ "$APPCENTER_BRANCH" != "master" ];
+then
+    plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/reactnativepipeline55/Info.plist
+fi
+plutil -replace CFBundleIdentifier -string "io.rangle.reactnativepipeline" $APPCENTER_SOURCE_DIRECTORY/reactnativepipeline55/Info.plist
